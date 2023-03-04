720cd930a1ad9f985130d48b8cc6b451ea7587d0
MateuszCharysz <charyszek@interia.pl>
2023-03-04 21:56:45 +0100

    working task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+'use strict';
+//import
+//help
+const log = console.log;
 
+//DOM
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.body;
+
+//CALLBACK// FUNCTIONS
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+const onClickStart = () => {
+  btnStart.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled'), '';
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+};
+
+const onClickStop = () => {
+  btnStop.setAttribute('disabled', '');
+  btnStart.removeAttribute('disabled', '');
+  clearInterval(timerId);
+};
+
+//EVENTS
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
