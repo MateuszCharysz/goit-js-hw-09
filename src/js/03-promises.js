f09714cb8d358317e225fef3307a38b675c9dded
MateuszCharysz <charyszek@interia.pl>
2023-03-09 00:01:19 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,29 +18,31 @@ log(delay);
 log(step);
 log(amount);
 //CALLBACK/FUNCTIONS
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    let onResolve = { position: `${position}`, stepTime: `${delay}` }; // Fulfill
-  } else {
-    let onReject = { position: `${position}`, stepTime: `${delay}` }; // Reject
-  }
-}
 
 const promiseLoader = (delay, step, amount) => {
-  let iterator = [...Array(amount)];
-  iterator.forEach()
+  const createPromise = (position, timeOfLunch) => {
+    const shouldResolve = Math.random() > 0.3;
+    new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve({ position: `${position}`, time: `${timeOfLunch}` }); // Fulfill
+      } else {
+        reject({ position: `${position}`, time: `${timeOfLunch}` }); // Reject
+      }
+    })
+    if (amount===position) clearInterval(intervalId);
+  };
+  const intervalId = setInterval(createPromise(), step);
+  setTimeout(intervalId, delay)
 
-  // const promise = new Promise((resolve, reject) => {
-  //   setTimeout(()=>{setInterval((createPromise()),step, delay,amount)},delay, step, amount)
-  // })
-  // createPromise()
-  //   .then(({ position, delay }) =>
-  //     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`),
-  //   )
-  //   .catch(({ position, delay }) =>
-  //     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`),
-  //   );
+
+
+  
+  // .then(() =>
+  //   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`),
+  // )
+  // .catch(() =>
+  //   Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`),
+  // );
 };
 // Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 // Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
@@ -49,6 +51,6 @@ const promiseLoader = (delay, step, amount) => {
 form.addEventListener('submit', event => {
   event.preventDefault();
   // log(delay.value, step.value, amount.value) // it works
-  log(typeof delay.value);
-  promiseLoader(delay.value, step.value, amount.value);
+  // log(typeof delay.value); valueAsNumber
+  promiseLoader(delay.valueAsNumber, step.valueAsNumber, amount.valueAsNumber);
 });
